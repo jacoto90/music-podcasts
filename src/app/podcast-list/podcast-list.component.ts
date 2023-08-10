577a67b5874051725ce9e9af42c2e71d72850c14
josea <joseangelcolmenatomas@gmail.com>
2023-08-10 17:24:15 +0200

    buscador de podcast OK

--- src/app/podcast-list/podcast-list.component.ts
+++ src/app/podcast-list/podcast-list.component.ts
@@ -9,12 +9,33 @@ import { PodcastService } from '../services/podcast.service';
 export class PodcastListComponent implements OnInit {
   podcasts: any[] = [];
   filterTerm: string = '';
+  searchTerm: string = '';
+  filteredPodcasts: any[] = []; // Inicialmente, la lista filtrada es igual a la lista original
 
   constructor(private podcastService: PodcastService) {}
-
   ngOnInit(): void {
-    this.podcastService.getTopPodcasts().subscribe(response => {
-      this.podcasts = response.feed.entry;  // Cambiado de response.results a response.feed.entry
-    });
+    this.podcastService.getTopPodcasts().subscribe(
+      (data: any) => {
+        if (data.feed && data.feed.entry) {
+          this.podcasts = data.feed.entry;
+          this.filteredPodcasts = [...this.podcasts]; // Inicialmente, la lista filtrada es igual a la lista completa
+        }
+      },
+      error => {
+        console.error('Error obteniendo podcasts:', error);
+      }
+    );
+  }
+
+  search() {
+    if (!this.searchTerm.trim()) {
+      // Si no hay término de búsqueda, muestra todos los podcasts.
+      this.filteredPodcasts = [...this.podcasts];
+      return;
+    }
+    // Filtrar los podcasts basado en el término de búsqueda.
+    this.filteredPodcasts = this.podcasts.filter(podcast =>
+      podcast['im:name'].label.toLowerCase().includes(this.searchTerm.toLowerCase())
+    );
   }
 }
