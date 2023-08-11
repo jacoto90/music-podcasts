ce5d63a825d75111191f4e54b19aa711bdb107c8
josea <joseangelcolmenatomas@gmail.com>
2023-08-11 14:20:48 +0200

    paguina de podcast details hecha

--- src/app/podcast-details/podcast-details.component.ts
+++ src/app/podcast-details/podcast-details.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PodcastService } from '../services/podcast.service';
 
 @Component({
   selector: 'app-podcast-details',
   templateUrl: './podcast-details.component.html',
   styleUrls: ['./podcast-details.component.scss']
 })
-export class PodcastDetailsComponent {
+export class PodcastDetailsComponent implements OnInit {
+  podcast: any;
 
+  constructor(private route: ActivatedRoute, private podcastService: PodcastService) { }
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      const podcastId = params['podcastId'];
+      this.podcastService.getPodcastById(podcastId).subscribe(data => {
+        this.podcast = data;
+      });
+    });
+  }
 }
--- src/app/services/podcast.service.ts
+++ src/app/services/podcast.service.ts
@@ -1,6 +1,7 @@
 import { Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 // Definición de la interfaz
 interface PodcastResponse {
@@ -19,5 +20,35 @@ export class PodcastService {
 
   getTopPodcasts(): Observable<PodcastResponse> {
     return this.http.get<PodcastResponse>(this.baseURL);
+  }
+
+  getPodcastById(podcastId: string): Observable<any> {
+    const url = `https://thingproxy.freeboard.io/fetch/https://itunes.apple.com/lookup?id=${podcastId}&media=podcast&entity=podcastEpisode&limit=20`;
+
+    return this.http.get(url).pipe(
+      map((response: any) => {
+        const podcastData = response.results[0];
+        const episodes = response.results.slice(1);
+        
+        return {
+          image: podcastData.artworkUrl600,
+          title: podcastData.collectionName,
+          artistName: podcastData.artistName, // Asumiendo que este es el campo correcto
+          description: podcastData.description,
+          episodes: episodes.map((episode: any) => ({
+            id: episode.trackId,
+            title: episode.trackName,
+            date: episode.releaseDate,
+            duration: this.convertMillisToMinutesAndSeconds(episode.trackTimeMillis)
+          }))
+        };
+      })
+    );
+  }
+  
+  convertMillisToMinutesAndSeconds(millis: number): string {
+    const minutes = Math.floor(millis / 60000);
+    const seconds = ((millis % 60000) / 1000).toFixed(0);
+    return `${minutes}:${seconds}`;
   }
 }
